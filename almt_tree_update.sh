17719e2073dbc06afb4394eb28e28ce210f8702d
Josh Singer <josh.singer@glasgow.ac.uk>
2020-02-07 11:06:06 +0000

    Alignment and tree update

--- almt_tree_update.sh
+++ almt_tree_update.sh
@@ -1,5 +1,3 @@
-git pull
-gluetools.sh -p log-level:FINEST -i run file covAlmtTreeUpdate.glue
 git add trees/gisaidUnconstrainedUnrooted.tree
 git add trees/gisaidMidpointRooted.tree
 git commit -m "Alignment and tree update"
