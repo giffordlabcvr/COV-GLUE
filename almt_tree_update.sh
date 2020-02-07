4c19ac10088abbb366bccabe6aa9f9060899223b
Josh Singer <josh.singer@glasgow.ac.uk>
2020-02-07 10:17:59 +0000

    added new sequences / metadata. Slighlty improved GLUE scripts for almt tree update etc.

--- almt_tree_update.sh
+++ almt_tree_update.sh
@@ -1,8 +1,5 @@
 git pull
-gluetools.sh -p log-level:FINEST -i run file covProject.glue
-gluetools.sh -p log-level:FINEST -i project cov run file glue/covRegenerateGisaidAlignment.glue
-gluetools.sh -p log-level:FINEST -i project cov run file glue/covGeneratePhylogeny.glue
-git add alignments/AL_GISAID_UNCONSTRAINED.fna
+gluetools.sh -p log-level:FINEST -i run file covAlmtTreeUpdate.glue
 git add trees/gisaidUnconstrainedUnrooted.tree
 git add trees/gisaidMidpointRooted.tree
 git commit -m "Alignment and tree update"
