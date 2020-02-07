31de998e0d02d3b5e2c4733111c4d9c95f45e23d
Josh Singer <josh.singer@glasgow.ac.uk>
2020-02-07 11:33:34 +0000

    script update

--- almt_tree_update.sh
+++ almt_tree_update.sh
@@ -1,4 +1,5 @@
 git add trees/gisaidUnconstrainedUnrooted.tree
 git add trees/gisaidMidpointRooted.tree
+git add alignments/AL_GISAID_UNCONSTRAINED.fna
 git commit -m "Alignment and tree update"
 git push
