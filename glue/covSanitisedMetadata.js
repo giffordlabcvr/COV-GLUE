deab263507734a4771ff836d0fbaec8a53a92236
Josh Singer <josh.singer@glasgow.ac.uk>
2020-03-28 12:27:03 +0000

    comments no longer needed in the script as they are in the README

--- glue/covSanitisedMetadata.js
+++ glue/covSanitisedMetadata.js
@@ -4,7 +4,6 @@ var tabularData = glue.command(["list", "sequence",
 	"source.name", 
 	"sequenceID", 
 	"length", 
-	// isolate == "virus name" in GISAID
     "isolate",
     "host_species",
     "collection_year",
@@ -18,27 +17,15 @@ var tabularData = glue.command(["list", "sequence",
 	"m49_country.m49_sub_region.display_name",
 	"m49_country.m49_region.display_name",
 	"gisaid_passage_details",
-	// GISAID QC mechanism: warn / info / normal
 	"gisaid_status_code", 
-	// GISAID QC comment
 	"gisaid_status_comment", 
 	"gisaid_originating_lab",
 	"gisaid_submitting_lab",
 	"gisaid_authors",
-	// true if various QC and metadata criteria are met
-	// if true, CoV-GLUE includes sequence in its analysis of AA replacements etc.
 	"analyse_variation", 
-	// ref_tree_candidate is a subset of analyse_variation, 
-	// if true, sequence may be selected to appear in the CoV-GLUE reference tree
 	"ref_tree_candidate", 
-	// if true, appears in the CoV-GLUE reference tree
 	"include_in_ref_tree", 
-	// for those sequences where include_in_ref_tree == true, 
-	// number of cluster which CD-HIT-EST assigned the sequence to
 	"cdhit_cluster", 
-	// most coarse-grained lineage assignment for the pandemic.
-	// if true, belongs to the clade characterised by amino acid L at ORF8 codon 84
-	// if false, belongs to the clade characterised by amino acid S at ORF8 codon 84
 	"is_l_lineage",
 	
 	]);
