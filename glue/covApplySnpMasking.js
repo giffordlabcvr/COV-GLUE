da65372a8d95c0f59034f5a4a34ddb560989120f
Josh Singer <josh.singer@glasgow.ac.uk>
2020-06-14 15:05:22 +0100

    updated comments.

--- glue/covApplySnpMasking.js
+++ glue/covApplySnpMasking.js
@@ -1,5 +1,10 @@
+// Apply SNPs to alignment from CSV file imported from the 
+// Rambaut et al. lineages repo.
 
+// reference coding spanning region
 var refNucleotides;
+
+// list of objects containing each sequence ID and the coding spanning region nucleotides
 var memberSeqObjs;
 
 glue.inMode("module/covFastaAlignmentExporterSourcePlusSeqId", function() {
@@ -18,6 +23,7 @@ _.each(memberSeqObjs, function(membSeqObj) {
 	var sourceName = bits[0];
 	var sequenceID = bits[1];
 	var snpsString;
+	// list the SNPs that have been recorded against the sequence.
 	glue.inMode("sequence/"+sourceName+"/"+sequenceID, function() {
 		snpsString = glue.command(["show", "property", "pang_masked_snps"]).propertyValueResult.value;
 	});
@@ -27,14 +33,17 @@ _.each(memberSeqObjs, function(membSeqObj) {
 	} else {
 		snps = snpsString.split(",");
 	}
+	// apply the SNP masking within the unconstrained alignment that is used for
+	// the reference tree generation.
 	glue.inMode("alignment/AL_GISAID_UNCONSTRAINED/member/"+sourceName+"/"+sequenceID, function() {
 		_.each(snps, function(snp) {
 			var snpRefNtChar = snp[0];
 			var snpMembNtChar = snp[snp.length-1];
 			var refCoord = parseInt(snp.substring(1, snp.length-1));
-			// check that the SNP proposed to be masked is correct.
+			// need to apply an offset here because of the coding spanning region.
 			var refNtChar = refNucleotides[refCoord-266];
 			var membNtChar = membSeqObj.sequence[refCoord-266];
+			// check that the SNP proposed to be masked is correct.
 			if(refNtChar != snpRefNtChar) {
 				glue.log("WARNING", "Sequence "+sourceName+"/"+sequenceID+": Reference character for SNP "+snp+" did not match. Expected "+snpRefNtChar+", found "+refNtChar)
 			}
